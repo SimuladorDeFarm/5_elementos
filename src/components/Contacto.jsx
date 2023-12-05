7dd4ab97ba6fade56a1a7c89eda46cf982760079
borisipe <boris.p.espinoza@gmail.com>
2023-12-05 00:44:12 -0300

    funcionalidad desc-serv1-serv2

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -14,14 +14,14 @@ function Contacto() {
                     <h4>+56946465775</h4>
                     <h4>email: 5elementos@gmail.cl</h4>
                 </div>
-                <div class="derecha" style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "30px", width: "100%", flexBasis: "content" }}>
+                <div class="derecha" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "30px", width: "100%", flexBasis: "content" }}>
                     <h4>Ubicación</h4>
                     <h4>Av. Cumming #322</h4>
                     <h4>Providencia, RM</h4>
                 </div>
             </div>
             <div class="row-2" style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "30px", backgroundColor: "rgba(106,90,206,1)" }}>
-                <div class="icons" style={{ display: "flex", flexDirection: "row" }}>
+                <div class="icons" style={{ display: "flex", flexDirection: "row", justifyContent: "space-between",inlineSize: "150px"}}>
                     <img src={facebook} width="30px"></img>
                     <img src={instagram} width="30px"></img>
                     <img src={whatsapp} width="30px"></img>
