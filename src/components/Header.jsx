874e4cd8f975866b85a1422ad7b46b983e5b1af6
Tomás Farías <83795131+SimuladorDeFarm@users.noreply.github.com>
2023-12-07 19:59:37 -0300

    bugFix navBarr
    -se arreglaorn los errores de la navBarr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,29 +14,21 @@ export const Header = () => {
         
     return (
         <>
-     <Navbar class="navbar navbar-expand-lg custom-navbar fixed-top vw-100" style={{ backgroundColor: "rgb(78, 61, 100)"}} >
-        <Navbar.Brand href="#" className='px-5'>
-          <img src={logo} width="60px" alt="Logo"></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse id="navbarNav">
-          <Nav className="navbar-nav">
-             <Nav.Link className='px-5'></Nav.Link>
-             <Nav.Link className='px-5'></Nav.Link>
-             <Nav.Link className='px-5'></Nav.Link>
-             <Nav.Link className='px-5'></Nav.Link>
-             <Nav.Link className='px-5'></Nav.Link>
-             <Nav.Link className='px-5'></Nav.Link>
-
-
-            <Nav.Link href="#descripcionEmpresa-id" className='px-3 text-light'>Inicio</Nav.Link>
-            <Nav.Link href="#descripcionEmpresa-id" className='px-3 text-light'>Agendar Hora</Nav.Link>
-            <Nav.Link href="#servicio-id" className='px-3 text-light'>Servicios</Nav.Link>
-            <Nav.Link href="#testimonio-id" className='px-3 text-light'>Testimonios</Nav.Link>
-            <Nav.Link href="#contacto-id" className='px-3 text-light'>Contacto</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+ <Navbar expand="lg" fixed="top" style={{ backgroundColor: "rgb(78, 61, 100)" }}>
+      <Navbar.Brand href="#" className='px-5'>
+        <img src={logo} width="60px" alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#descripcionEmpresa-id" className='px-3 text-light'>Inicio</Nav.Link>
+          <Nav.Link href="#descripcionEmpresa-id" className='px-3 text-light'>Agendar Hora</Nav.Link>
+          <Nav.Link href="#servicio-id" className='px-3 text-light'>Servicios</Nav.Link>
+          <Nav.Link href="#testimonio-id" className='px-3 text-light'>Testimonios</Nav.Link>
+          <Nav.Link href="#contacto-id" className='px-3 text-light'>Contacto</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
       {/* Contenido de las secciones */}
 
     </>
